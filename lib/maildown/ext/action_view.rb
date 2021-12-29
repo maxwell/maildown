f202b3402187e2c362f61811a7b88b1f90100ac3
Maxwell Salzberg <maxwell@backerkit.com>
2021-12-29 12:55:08 -0800

    yolo try removing

--- lib/maildown/ext/action_view.rb
+++ lib/maildown/ext/action_view.rb
@@ -2,19 +2,19 @@
 
 # This monkeypatch allows the use of `.md.erb` file extensions
 # in addition to `.md+erb` and `.md`
-module ActionView
-  class FileSystemResolver
-    alias :original_extract_handler_and_format_and_variant :extract_handler_and_format_and_variant
-
-    # Different versions of rails have different
-    # method signatures here, path is always first
-    def extract_handler_and_format_and_variant(*args)
-      if args.first.end_with?('md.erb')
-        path = args.shift
-        path = path.gsub(/\.md\.erb\z/, '.md+erb')
-        args.unshift(path)
-      end
-      return original_extract_handler_and_format_and_variant(*args)
-    end
-  end
-end
+# module ActionView
+#   class FileSystemResolver
+#     alias :original_extract_handler_and_format_and_variant :extract_handler_and_format_and_variant
+#
+#     # Different versions of rails have different
+#     # method signatures here, path is always first
+#     def extract_handler_and_format_and_variant(*args)
+#       if args.first.end_with?('md.erb')
+#         path = args.shift
+#         path = path.gsub(/\.md\.erb\z/, '.md+erb')
+#         args.unshift(path)
+#       end
+#       return original_extract_handler_and_format_and_variant(*args)
+#     end
+#   end
+# end
